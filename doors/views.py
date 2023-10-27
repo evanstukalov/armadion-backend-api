800f9ed8db9a82c0e6feff6ef7aab55b5035be1d
evan <comrade.codredi@gmail.com>
2023-10-27 12:33:28 +0300

    refactor: decompose viewset on different views

--- doors/views.py
+++ doors/views.py
@@ -1,19 +1,27 @@
 
 from doors.models import Door
-from doors.serializer import MainPageCatalogSerializer
+from doors.serializer import MainPageCatalogSerializer, DetailViewSerializer
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework import viewsets
-from rest_framework.response import Response
 from rest_framework import filters
+from rest_framework import generics
 
-
-class DoorViewSet(viewsets.ReadOnlyModelViewSet):
+class MainPageDoorsAPIView(generics.ListAPIView):
     """
-    A viewset that provides default `list` and `retrieve` actions.
+    API View for the main page that provides doors list.
     """
     queryset = Door.objects.all()
     serializer_class = MainPageCatalogSerializer
     pagination_class = LimitOffsetPagination
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['click_counter']
+
+
+class DetailViewDoorsAPIView(generics.RetrieveAPIView):
+    """
+    API View for the detail page that provides doors detail.
+    """
+    queryset = Door.objects.all()
+    serializer_class = DetailViewSerializer
+
+
 
