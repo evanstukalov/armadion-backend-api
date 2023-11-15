1ce6037fc4c69950c6e0685e79caddb4d231ca9e
evan <evanstukalov@gmail.com>
2023-11-15 12:02:07 +0300

    chore: not caching if debug == 1

--- armadion/settings.py
+++ armadion/settings.py
@@ -101,16 +101,22 @@ DATABASES = {
     }
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-
+if DEBUG:
+   CACHES = {
+       'default': {
+           'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+       }
+   }
+else:
+   CACHES = {
+       "default": {
+           "BACKEND": "django_redis.cache.RedisCache",
+           "LOCATION": "redis://redis:6379/1",
+           "OPTIONS": {
+               "CLIENT_CLASS": "django_redis.client.DefaultClient",
+           }
+       }
+   }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
